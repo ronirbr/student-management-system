e3d9f6b3c352324afabedbeef77ad9b3cb56802d
ronirbr <roni.ribeiro.n00@gmail.com>
2023-01-04 15:46:44 -0300

    insert method

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -10,9 +10,20 @@ class Home extends Controller
     // code...
     $user = new User();
 
-    $data = $user->findAll();
 
     //$data = $user->where('firstname', 'john');
+    $arr['firstname'] = 'Boom';
+    $arr['lastname'] = 'Bam';
+    $arr['date'] = '2023-01-02 13:52:11';
+    $arr['user_id'] = 'sdsdsds';
+    $arr['gender'] = 'female';
+    $arr['school_id'] = 'sdsdsdsd';
+    $arr['level'] = 'student';
+
+    $user->insert($arr);
+    //$user->update(id, $data);
+    //$user->delete(id); 
+    $data = $user->findAll();
 
     $this->view('home', ['rows'=>$data]);
   }
--- private/core/model.php
+++ private/core/model.php
@@ -29,4 +29,34 @@ class Model extends Database
     $query = "select * from $this->table";
     return $this->query($query);
   }
+
+  public function insert($data)
+  {
+
+    $keys = array_keys($data);
+    $columns = implode(',', $keys);
+    $values = implode(',:', $keys);
+
+    $query = "insert into $this->table ($columns) values (:$values)";
+
+    return $this->query($query, $data);
+  }
+
+  public function update($id, $data)
+  {
+    $column = addslashes($column);
+    $query = "select * from $this->table where $column = :value";
+    return $this->query($query, [
+      'value' => $value
+    ]);
+  }
+
+  public function dalete($id)
+  {
+    $column = addslashes($column);
+    $query = "select * from $this->table where $column = :value";
+    return $this->query($query, [
+      'value' => $value
+    ]);
+  }
 }
